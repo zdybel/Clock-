ca43f13d98d4c91251a37a5fbc59ab0121e06f28
Brynne Zdybel <bazdybel@gmail.com>
2016-11-19 23:46:13 -0500

    pictures change every 10 seconds

--- clock.js
+++ clock.js
@@ -24,21 +24,21 @@ function getZero(i) {
 function changeImage(i) {
 	var image = document.getElementById("img");
 	if (i >= 0 && i < 10){
-		image.style.backgroundImage = "red";
+		image.style.backgroundImage = "url('https://s-media-cache-ak0.pinimg.com/originals/c6/28/0e/c6280e8f909cdaa96f599f4b611e876c.jpg')";
 	}
 	if (i >= 10 && i < 20){
-		image.style.backgroundColor = "orange";
+		image.style.backgroundImage = "url('http://dreamatico.com/data_images/stars/stars-2.jpg')";
 	}
 	if (i >= 20 && i < 30){
-		image.style.backgroundColor = "yellow";
+		image.style.backgroundImage = "url('http://img00.deviantart.net/8dea/i/2016/230/5/1/ocean__stars__sky__and_you_by_muddymelly-d4bg1ub.jpg')";
 	}
 	if (i >= 30 && i < 40){
-		image.style.backgroundColor = "green";
+		image.style.backgroundImage = "url('https://s-media-cache-ak0.pinimg.com/originals/c9/d7/ce/c9d7ce2bd08d1179fefdcbebfe56ce16.jpg')";
 	}
 	if (i >= 40 && i < 50){
-		image.style.backgroundColor = "blue";
+		image.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/M101_hires_STScI-PRC2006-10a.jpg/1280px-M101_hires_STScI-PRC2006-10a.jpg')";
 	}
 	if (i >= 50 && i < 60){
-		image.style.backgroundColor = "purple";
+		image.style.backgroundImage = "url('http://www.space.com/images/i/000/019/091/original/july-skywatching-portland.jpg?interpolation=lanczos-none&downsize=*:1000')";
 	}
 }
